87360a18a35e13783e8b4b86d5122f6370e49e60
Aditya Agarwal <50960175+adi611@users.noreply.github.com>
2022-06-21 22:15:49 +0530

    add post

--- src/components/CompanyVerification/index.js
+++ src/components/CompanyVerification/index.js
@@ -5,83 +5,187 @@ import theme from "../../theme/theme";
 import { ThemeProvider, Box, Grid, Button, TextField, makeStyles } from '@material-ui/core';
 import OtpInput from 'react-otp-input';
 import PhoneInput from 'react-phone-input-2'
+import axios from 'axios';
 // import FileUpload from 'react-material-file-upload';
 import 'react-phone-input-2/lib/material.css'
 
 export default props => {
-    const [phone, setPhone] = useState()
-    const [otp, setOtp] = useState()
-    const [otp2, setOtp2] = useState()
-    const [files, setFiles] = useState([])
+    // const [phone, setPhone] = useState()
+    // const [otp, setOtp] = useState()
+    // const [otp2, setOtp2] = useState()
+    // const [files, setFiles] = useState([])
+    const [formData, setFormData] = useState({
+        phone: null,
+        otpPhone: '',
+        email: '',
+        otpEmail: '',
+        fileUrl: '',
+        pan: '',
+        aadhar: '',
+        address: '',
+        gst: ''
+    })
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        let data = {}
+
+        data.panCard = formData.pan
+        data.registrationCertficate = formData.fileUrl
+
+        const url = 'http://localhost:8080/api/company/create'
+        const config = {
+            headers: {
+                'content-type': 'application/json',
+            }
+        }
+        axios.post(url, data, config).then((response) => {
+            console.log(response.data);
+        })
+        console.log(formData);
+    }
+
+    const handleInput = e => {
+        const name = e.target.name;
+        const newValue = e.target.value;
+        // console.log(name, newValue)
+        setFormData(d => ({ ...d, [name]: newValue }));
+    };
+
+    const handlePhoneChange = (value, country, event) => {
+        setFormData({
+            ...formData,
+            'phone': value,
+        });
+    };
+
+
+    // const setUrl = url => {
+    //     setFormData(d => ({ ...d, fileUrl: url }));
+    // }
 
     return (
 
         <ThemeProvider theme={theme}>
             <Header title="Company verification" />
             <Box p={8}>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <PhoneInput
-                            placeholder="Enter phone number"
-                            country="in"
-                            value={phone}
-                            onChange={setPhone} />
-                        <Box mt={2}>
-                            <Button variant="outlined">Get OTP</Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="outlined-basic" label="Enter PAN card no." variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box style={{ display: "flex", gap: "1rem" }}>
-                            <p>Enter OTP</p>
-                            <OtpInput
-                                value={otp}
-                                onChange={setOtp}
-                                numInputs={4}
-                                separator={<span>-</span>}
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <PhoneInput
+                                placeholder="Enter phone number"
+                                country="in"
+                                value={formData.phone}
+                                onChange={handlePhoneChange}
                             />
-                        </Box>
-                        {/* <TextField id="outlined-basic" label="Enter OTP" variant="outlined" fullWidth /> */}
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="outlined-basic" label="Enter AADHAR card no." variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField autoComplete="email" id="outlined-basic" label="Company Email Address" variant="outlined" fullWidth />
-                        <Box mt={2}>
-                            <Button variant="outlined">Get OTP</Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="outlined-basic" label="GST no." variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Box style={{ display: "flex", gap: "1rem" }}>
-                            <p>Enter OTP</p>
-                            <OtpInput
-                                value={otp2}
-                                onChange={setOtp2}
-                                numInputs={6}
-                                separator={<span>-</span>}
+                            <Box mt={2}>
+                                <Button variant="outlined">Get OTP</Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Enter PAN card no."
+                                variant="outlined"
+                                fullWidth
+                                name="pan"
+                                value={formData.pan}
+                                onChange={handleInput}
                             />
-                        </Box>
-                        {/* <TextField id="outlined-basic" label="Enter OTP" variant="outlined" fullWidth /> */}
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Box style={{ display: "flex", gap: "1rem" }}>
+                                <p>Enter OTP</p>
+                                <OtpInput
+                                    name="otpPhone"
+                                    value={formData.otpPhone}
+                                    onChange={setFormData}
+                                    numInputs={4}
+                                    separator={<span>-</span>}
+                                />
+                            </Box>
+                            {/* <TextField id="outlined-basic" label="Enter OTP" variant="outlined" fullWidth /> */}
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Enter AADHAR card no."
+                                variant="outlined"
+                                fullWidth
+                                name="aadhar"
+                                value={formData.aadhar}
+                                onChange={handleInput}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                autoComplete="email"
+                                id="outlined-basic"
+                                label="Company Email Address"
+                                variant="outlined"
+                                fullWidth
+                                name="email"
+                                value={formData.email}
+                                onChange={handleInput}
+                            />
+                            <Box mt={2}>
+                                <Button variant="outlined">Get OTP</Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="outlined-basic"
+                                label="GST no."
+                                variant="outlined"
+                                fullWidth
+                                name="gst"
+                                value={formData.gst}
+                                onChange={handleInput}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Box style={{ display: "flex", gap: "1rem" }}>
+                                <p>Enter OTP</p>
+                                <OtpInput
+                                    name="otpEmail"
+                                    value={formData.otpEmail}
+                                    onChange={handleInput}
+                                    numInputs={6}
+                                    separator={<span>-</span>}
+                                />
+                            </Box>
+                            {/* <TextField id="outlined-basic" label="Enter OTP" variant="outlined" fullWidth /> */}
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="outlined-multiline-static"
+                                rows={4}
+                                multiline
+                                label="Company Address"
+                                variant="outlined"
+                                fullWidth
+                                name="address"
+                                value={formData.address}
+                                onChange={handleInput}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            {/* <FileUpload value={files} onChange={setFiles} /> */}
+                            {/* <input type="file" onChange={(e) => setFiles(e.target.files)} /> */}
+                            <p>Company Registration Document</p>
+                            <FileUpload
+                                name="fileUrl"
+                                value={formData.fileUrl}
+                                setUrl = {setFormData}
+                                onChange={handleInput}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <TextField id="outlined-multiline-static" rows={4} multiline label="Company Address" variant="outlined" fullWidth />
-                    </Grid>
-                    <Grid item xs={6}>
-                        {/* <FileUpload value={files} onChange={setFiles} /> */}
-                        {/* <input type="file" onChange={(e) => setFiles(e.target.files)} /> */}
-                        <p>Company Registration Document</p>
-                        <FileUpload />
-                    </Grid>
-                </Grid>
-                <Box textAlign='center'>
-                    <Button variant="contained" color="primary" style={{ marginTop: "4rem" }}>Verify</Button>
-                </Box>
+                    <Box textAlign='center'>
+                        <Button type="submit" variant="contained" color="primary" style={{ marginTop: "4rem" }}>Submit</Button>
+                    </Box>
+                </form>
             </Box>
         </ThemeProvider>
     )
--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
+// import { uploadFile } from 'react-s3';
+// import S3 from 'react-aws-s3';
+import AWS from 'aws-sdk';
 import { Button } from '@material-ui/core';
 import axios from 'axios';
 
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
 export default props => {
+    AWS.config.update({ region: process.env.REACT_APP_REGION, credentials: new AWS.Credentials(process.env.REACT_APP_ACCESS_KEY, process.env.REACT_APP_SECRET_ACCESS_KEY) });
+    var s3 = new AWS.S3({ apiVersion: "2006-03-01", params: { Bucket: process.env.REACT_APP_S3_BUCKET } });
+
+
+    const config = {
+        bucketName: process.env.REACT_APP_S3_BUCKET,
+        region: process.env.REACT_APP_REGION,
+        accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+        secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+    }
 
     const [file, setFile] = useState()
 
@@ -10,28 +25,53 @@ export default props => {
         setFile(event.target.files[0])
     }
 
-    function handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault()
-        const url = 'http://localhost:8081/upload';
-        const formData = new FormData();
-        formData.append('resume', file);
-        formData.append('fileName', file.name);
-        const config = {
-            headers: {
-                'content-type': 'multipart/form-data',
-            },
-        };
-        axios.post(url, formData, config).then((response) => {
-            console.log(response.data);
-        });
-
+        event.stopPropagation()
+        console.log(file.name);
+        let upload_params = { Bucket: process.env.REACT_APP_S3_BUCKET, Key: file.name, Body: file };
+        let upload = new AWS.S3.ManagedUpload({ params: upload_params });
+        let promise = upload.promise();
+        promise.then(
+            function (data) { props.setUrl(d => ({ ...d, "fileUrl": data.Location })) ; },
+            function (err) { console.log("Failed to upload", file.name, "with error:", err.message); }
+        );
+        // event.preventDefault()
+        // const url = 'http://localhost:8081/upload';
+        // const formData = new FormData();
+        // formData.append('resume', file);
+        // formData.append('fileName', file.name);
+        // const config = {
+        //     headers: {
+        //         'content-type': 'multipart/form-data',
+        //     },
+        // };
+        // axios.post(url, formData, config).then((response) => {
+        //     console.log(response.data);
+        // });
     }
+
+    //     uploadFile(file, config)
+    //         .then(data => console.log(data))
+    //         .catch(err => console.error(err))
+
+    //     // props.setUrl(d => ({ ...d, "fileUrl": data.location })) ;
+
+
+    // const uploadFile = async (file) => {
+    //     const ReactS3Client = new S3(config);
+    //     // the name of the file uploaded is used to upload it to S3
+    //     ReactS3Client
+    //         .uploadFile(file, file.name)
+    //         .then(data => console.log(data.location))
+    //         .catch(err => console.error(err))
+    // }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="file" onChange={handleChange} />
-                <button type="submit">Upload</button>
+                <button type='submit'>Upload</button>
                 {/* <Button
                     variant="contained"
                     component="label"
